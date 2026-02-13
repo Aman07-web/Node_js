a16b35206f8c5047c5c1909c6e70b6c906794944
Aman dhoni <parleaman@gmail.com>
2026-02-13 14:10:19 +0530

    getting request from post

--- createServer.js
+++ createServer.js
@@ -1,6 +1,30 @@
+const { log } = require('console');
+
 const http=require('http');
+const fs=require('fs');
+const user={
+  name:"aman kumar",
+  age:21
+}
 
 http.createServer((req,resp)=>{
-  resp.write("hello node js");
+  if(req.method==="GET" && req.url==="/user"){
+    resp.writeHead(200,{"Content-Type": "application/json"});
+    resp.write(JSON.stringify(user));
+  }
+  else if(req.method==="POST" && req.url==="/user"){
+    let body="";
+    req.on("data",(chunk)=>{
+      body+=chunk;
+    })
+    req.on("end", ()=>{
+     fs.writeFile("data.txt",body, ()=>{
+      resp.writeHead(200)
+      resp.end("done");
+     })
+    })
+  
+  }
   resp.end();
-}).listen(4000);
+}).listen(3000,()=>{console.log("Server running on http://localhost:3000");
+});

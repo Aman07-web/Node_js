6bf6a9d3e5fd3494df2044930fa1332f9ab40a94
Aman dhoni <parleaman@gmail.com>
2026-03-09 09:38:20 +0530

    all method of path

--- path.js
+++ path.js
@@ -1,2 +1,14 @@
 const path=require("path");
-const info = path.parse("/users/abhi/file.txt");console.log(info);
+
+//const info = path.parse("/users/abhi/file.txt");console.log(info);
+
+const filePath=path.join("user","aman","cricket.txt");
+console.log(filePath);
+
+const parsePath=path.parse(filePath);
+const extPath=path.extname(filePath);
+const resolve=path.resolve(filePath);
+const dirPath=path.dirname(filePath);
+const baseName=path.basename(filePath);
+
+console.log({parsePath,extPath,resolve,dirPath,baseName});

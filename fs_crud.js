a7f0b24f7ad3eeeee969d2318ead63deeeae9bac
Aman dhoni <parleaman@gmail.com>
2026-03-21 21:48:47 +0530

    crud operation

---
+++ fs_crud.js
@@ -0,0 +1,9 @@
+const fs=require('fs');
+
+//fs.writeFileSync("Node_js/file/apple.txt","this file related to apple")// create file
+//fs.unlinkSync("Node_js/file/apple.txt")
+
+//const data=fs.readFileSync("Node_js/file/apple.txt","utf-8");
+//console.log(data);
+
+//fs.appendFileSync("Node_js/file/apple.txt","hii, aman kumar")
